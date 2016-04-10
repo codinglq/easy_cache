51ebdb6caf6fd8297d69abfe25fc11899589d033
codinglq <704788249@qq.com>
2016-04-10 21:32:34 +0800

    封装外部使用的查询和插入结构体

--- hash_table/hash_table.h
+++ hash_table/hash_table.h
@@ -14,6 +14,21 @@ typedef struct hash_table
     unsigned long max_cache_table_size;
 }CacheHashTable;
 
+/*每个out item被插入完成或者get完成之后就会被释放
+*这样的结构会方便网络传输和节省空间
+* */
+typedef struct out_cache_item
+{
+    /*过期时间*/
+    int out_time;
+    /*key的偏移量*/
+    int key_offset;
+    /*value基于key的偏移量*/
+    int value_offset;
+    /*存储key和value的数组*/
+    char key_value_buf[0]
+}OutCacheItem;
+
 /*生成随机字符串*/
 char * random_str(int str_len);
 
@@ -38,4 +53,18 @@ CacheItem* get_cache_hash_table(CacheHashTable *cache_hash_table_p,const char * 
 /*从hash table中删除一个KV*/
 int delete_cache_hash_table_item_by_key(CacheHashTable * cache_hash_table_p,const char *key_p);
 
+/*获取key*/
+char * get_key(OutCacheItem *out_cache_item_p);
+
+/*获取value*/
+char * get_value(OutCacheItem * out_cache_item_p);
+
+/*获取outtime*/
+int get_out_time(OutCacheItem *out_);
+
+/*向hashtable中插入一个put元素*/
+int put(CacheHashTable *cache_hash_table_p,OutCacheItem *out_cache_item_p);
+
+/*在hashtable中取出一个key为传入的元素*/
+CacheItem * get(CacheHashTable *cache_hash_table_p,OutCacheItem *out_cache_item_p);
 #endif
